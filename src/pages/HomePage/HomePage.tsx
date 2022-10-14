7b58cebc1ecfa8af78b151b1f7c5fddedad77dd0
Augusto Lopes <augustolfp@gmail.com>
2022-10-14 15:32:45 -0300

    feat(homepage): Add Links to navigate on app

--- src/pages/HomePage/HomePage.tsx
+++ src/pages/HomePage/HomePage.tsx
@@ -1,3 +1,18 @@
+import styled from 'styled-components'
+import { Link } from 'react-router-dom'
+
 export default function HomePage() {
-    return <h1>Home page!</h1>
+    return (
+        <Container>
+            <h1>Home page!</h1>
+            <Link to="/dashboard">Dashboard</Link>
+            <Link to="/sign-in">Sign-in</Link>
+            <Link to="sign-up">Sign-up</Link>
+        </Container>
+    )
 }
+
+const Container = styled.div`
+    display: flex;
+    flex-direction: column;
+`
