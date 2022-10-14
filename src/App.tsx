cf90930661a64bc05ee61d94aa7049da94cb0c9d
Augusto Lopes <augustolfp@gmail.com>
2022-10-14 15:25:45 -0300

    feat(homepage): Create homepage file

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,20 @@
-import { Routes, Route } from "react-router-dom";
-import DayPage from "./pages/DayPage/DayPage";
-import HomePage from "./pages/HomePage/HomePage";
-import SignInPage from "./pages/SignInPage/SignInPage";
-import SignUpPage from "./pages/SignUpPage/SignUpPage";
+import { Routes, Route } from 'react-router-dom'
+import DayPage from './pages/DayPage/DayPage'
+import DashBoard from './pages/DashBoard/DashBoard'
+import SignInPage from './pages/SignInPage/SignInPage'
+import SignUpPage from './pages/SignUpPage/SignUpPage'
+import HomePage from './pages/HomePage/HomePage'
 
 function App() {
-  return (
-    <Routes>
-      <Route path="/" element={<HomePage />} />
-      <Route path="/sign-in" element={<SignInPage />} />
-      <Route path="/sign-up" element={<SignUpPage />} />
-      <Route path="/day/:id" element={<DayPage />} />
-    </Routes>
-  );
+    return (
+        <Routes>
+            <Route path="/" element={<HomePage />} />
+            <Route path="/dashboard" element={<DashBoard />} />
+            <Route path="/sign-in" element={<SignInPage />} />
+            <Route path="/sign-up" element={<SignUpPage />} />
+            <Route path="/day/:id" element={<DayPage />} />
+        </Routes>
+    )
 }
 
-export default App;
+export default App
