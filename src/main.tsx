be1f32ea479158e4200f164f16c198b7eaf6c56e
Augusto Lopes <augustolfp@gmail.com>
2022-10-17 14:17:05 -0300

    feat(data-fetching): Implement data fetching using react Query

--- src/main.tsx
+++ src/main.tsx
@@ -4,13 +4,17 @@ import App from './App';
 import './assets/styles/reset.css';
 import { BrowserRouter } from 'react-router-dom';
 import { AuthProvider } from './contexts/AuthContext';
+import { QueryClientProvider } from 'react-query';
+import { queryClient } from './services/queryClient';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
     <React.StrictMode>
         <BrowserRouter>
-            <AuthProvider>
-                <App />
-            </AuthProvider>
+            <QueryClientProvider client={queryClient}>
+                <AuthProvider>
+                    <App />
+                </AuthProvider>
+            </QueryClientProvider>
         </BrowserRouter>
     </React.StrictMode>
 );
--- src/pages/DashBoard/DashBoard.tsx
+++ src/pages/DashBoard/DashBoard.tsx
@@ -1,3 +1,71 @@
+import { useQuery } from 'react-query';
+import { api } from '../../lib/axios';
+import { useAuth } from '../../hooks/useAuth';
+import styled from 'styled-components';
+
+type MealIngredient = {
+    id: number;
+    name: string;
+    mealId: number;
+    weightInGrams: number;
+    carbsInGrams: number;
+    fatsInGrams: number;
+    proteinsInGrams: number;
+    kcals: number;
+};
+
+type DayMeal = {
+    mealId: number;
+    countedDayId: number;
+    mealName: string;
+    mealDescription: string;
+    mealCreationDate: string;
+    carbs: number;
+    fats: number;
+    proteins: number;
+    kcals: number;
+    ingredientList: MealIngredient[];
+};
+
+type Day = {
+    id: number;
+    day: string;
+    notes: string;
+    userId: number;
+    caloriesTarget: number;
+    proteinsTarget: number;
+    carbs: number;
+    fats: number;
+    proteins: number;
+    kcals: number;
+    dayMeals: DayMeal[];
+};
+
 export default function DashBoard() {
-    return <h1>DashBoard!</h1>
+    const { token } = useAuth();
+    const { data, isFetching } = useQuery<Day[]>(
+        'userData',
+        async () => {
+            const response = await api.get('/get-days-data', token);
+
+            return response.data;
+        },
+        {
+            staleTime: 1000 * 60, // 1 minute
+        }
+    );
+    return (
+        <Container>
+            {isFetching && <p>Carregando...</p>}
+            {data?.map((day, index) => {
+                return (
+                    <div key={index}>
+                        <h1>{day.notes}</h1>
+                    </div>
+                );
+            })}
+        </Container>
+    );
 }
+
+const Container = styled.div``;
