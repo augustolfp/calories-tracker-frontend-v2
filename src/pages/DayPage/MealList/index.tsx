593161c0af8522a5108cfe0a0d408ccffd6527ad
Augusto Lopes <augustolfp@gmail.com>
2022-10-30 13:58:15 -0300

    feat(Day meals): Implement tabs to organize meals

--- src/pages/DayPage/MealList/index.tsx
+++ src/pages/DayPage/MealList/index.tsx
@@ -1,12 +1,21 @@
 import MealDash from './MealDash';
+import { Tabs, TabList, TabPanels, Tab, TabPanel, Box } from '@chakra-ui/react';
 
 export default function MealList(props: any) {
     return (
-        <>
-            {Object.keys(props).map((key) => {
-                const meal = props[key];
-                return <MealDash key={key} {...meal} />;
-            })}
-        </>
+        <Tabs>
+            <TabList>
+                {Object.keys(props).map((key) => (
+                    <Tab key={key}>{props[key].mealName}</Tab>
+                ))}
+            </TabList>
+            <TabPanels>
+                {Object.keys(props).map((key) => (
+                    <TabPanel key={key}>
+                        <MealDash {...props[key]} />
+                    </TabPanel>
+                ))}
+            </TabPanels>
+        </Tabs>
     );
 }
