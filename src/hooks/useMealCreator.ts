c90c03f8d15aed3063b24e6ff794fc1385fc10f9
Augusto Lopes <augustolfp@gmail.com>
2022-11-08 16:01:18 -0300

    feat(Add meals): Add status toasts to meal creation

--- src/hooks/useMealCreator.ts
+++ src/hooks/useMealCreator.ts
@@ -2,10 +2,13 @@ import { useMutation, useQueryClient } from 'react-query';
 import { useAuth } from './useAuth';
 import { api } from '../lib/axios';
 import { MealBody } from '../types';
+import { useToast } from '@chakra-ui/react';
+import { AxiosError } from 'axios';
 
 export function useMealCreator() {
     const { token } = useAuth();
     const queryClient = useQueryClient();
+    const toast = useToast();
 
     const addMeal = async (data: any) => {
         return await api.post('/add-meal', data, token);
@@ -15,8 +18,28 @@ export function useMealCreator() {
         mutationFn: (mealBody: MealBody) => {
             return api.post('/add-meal', mealBody, token);
         },
-        onSettled: () => {
-            queryClient.invalidateQueries('userData');
+        onSuccess: () => {
+            toast({
+                title: 'Que delícia, cara',
+                description: 'Refeição adicionada com sucesso!',
+                status: 'success',
+                duration: 4000,
+                isClosable: true,
+            });
+            return queryClient.invalidateQueries('userData');
+        },
+        onError: (err: AxiosError) => {
+            toast({
+                title: 'Ocorreu um erro :(',
+                description: `${
+                    err.response?.data
+                        ? err.response.data
+                        : 'Erro desconhecido. Atualize a página!'
+                }`,
+                status: 'error',
+                duration: 4000,
+                isClosable: true,
+            });
         },
     });
 }
