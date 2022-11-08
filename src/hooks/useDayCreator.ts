b80e84565c7d18de5bc572eb34a0ce8154255b5f
Augusto Lopes <augustolfp@gmail.com>
2022-11-08 15:55:45 -0300

    feat(Add counted day): Add status toasts to counted day creation

--- src/hooks/useDayCreator.ts
+++ src/hooks/useDayCreator.ts
@@ -2,17 +2,40 @@ import { useMutation, useQueryClient } from 'react-query';
 import { useAuth } from './useAuth';
 import { api } from '../lib/axios';
 import { DayBody } from '../types';
+import { useToast } from '@chakra-ui/react';
+import { AxiosError } from 'axios';
 
 export function useDayCreator() {
     const { token } = useAuth();
     const queryClient = useQueryClient();
+    const toast = useToast();
 
     return useMutation({
         mutationFn: (dayBody: DayBody) => {
             return api.post('/add-counted-day', dayBody, token);
         },
-        onSettled: () => {
-            queryClient.invalidateQueries('userData');
+        onSuccess: () => {
+            toast({
+                title: 'Birl!',
+                description: 'Dia adicionado com sucesso!',
+                status: 'success',
+                duration: 4000,
+                isClosable: true,
+            });
+            return queryClient.invalidateQueries('userData');
+        },
+        onError: (err: AxiosError) => {
+            toast({
+                title: 'Ocorreu um erro :(',
+                description: `${
+                    err.response?.data
+                        ? err.response.data
+                        : 'Erro desconhecido. Atualize a página!'
+                }`,
+                status: 'error',
+                duration: 4000,
+                isClosable: true,
+            });
         },
     });
 }
