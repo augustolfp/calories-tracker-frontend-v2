a55cb30bbf40e7ea072ba880e6edeb812be24af1
Augusto Lopes <augustolfp@gmail.com>
2022-11-08 16:05:42 -0300

    feat(Add ingredients): Add status toasts to ingredient creation

--- src/hooks/useIngredientCreator.ts
+++ src/hooks/useIngredientCreator.ts
@@ -2,17 +2,40 @@ import { useMutation, useQueryClient } from 'react-query';
 import { useAuth } from './useAuth';
 import { api } from '../lib/axios';
 import { IngBody } from '../types';
+import { useToast } from '@chakra-ui/react';
+import { AxiosError } from 'axios';
 
 export function useIngredientCreator() {
     const { token } = useAuth();
     const queryClient = useQueryClient();
+    const toast = useToast();
 
     return useMutation({
         mutationFn: (ingBody: IngBody) => {
             return api.post('/add-ingredients', ingBody, token);
         },
-        onSettled: () => {
-            queryClient.invalidateQueries('userData');
+        onSuccess: () => {
+            toast({
+                title: 'Ai papai',
+                description: 'Ingrediente adicionado com sucesso!',
+                status: 'success',
+                duration: 4000,
+                isClosable: true,
+            });
+            return queryClient.invalidateQueries('userData');
+        },
+        onError: (err: AxiosError) => {
+            toast({
+                title: 'Ocorreu um erro :(',
+                description: `${
+                    err.response?.data
+                        ? err.response.data
+                        : 'Erro desconhecido. Atualize a página!'
+                }`,
+                status: 'error',
+                duration: 4000,
+                isClosable: true,
+            });
         },
     });
 }
