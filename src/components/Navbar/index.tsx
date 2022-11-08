954e9d5d79482bce3f8155672cf4a192727e761c
Augusto Lopes <augustolfp@gmail.com>
2022-11-08 16:38:05 -0300

    feat(NavBar): Add username to navbar

--- src/components/Navbar/index.tsx
+++ src/components/Navbar/index.tsx
@@ -1,17 +1,20 @@
+import { useAuth } from '../../hooks/useAuth';
 import { Box, Flex, useColorModeValue } from '@chakra-ui/react';
 import ToggleThemeBtn from './ToggleThemeBtn';
 
 export default function NavBar() {
+    const { isLoggedIn, userName } = useAuth();
     const navBackground = useColorModeValue('purple.100', 'purple.700');
 
     return (
-        <Box
+        <Flex
             as="nav"
             bg={navBackground}
             boxShadow={useColorModeValue('sm', 'sm-dark')}
             padding="6px"
         >
+            <Box>{userName ? userName : null}</Box>
             <ToggleThemeBtn />
-        </Box>
+        </Flex>
     );
 }
