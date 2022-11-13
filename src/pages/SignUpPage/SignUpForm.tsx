818b830da113363535888c08dcf8881eb70f72f8
Augusto Lopes <augustolfp@gmail.com>
2022-11-13 16:24:04 -0300

    feat(sign-up): Add styling to sign-up page

--- src/pages/SignUpPage/SignUpForm.tsx
+++ src/pages/SignUpPage/SignUpForm.tsx
@@ -5,8 +5,7 @@ import {
     FormLabel,
     CircularProgress,
     Box,
-    Flex,
-    Heading,
+    VStack,
 } from '@chakra-ui/react';
 
 type Props = {
@@ -24,90 +23,85 @@ type Props = {
 
 export default function SignUpForm(props: Props) {
     return (
-        <Flex width="full" align="center" justifyContent="center">
-            <Box maxWidth="500px" layerStyle="card">
-                <Box textAlign="center" m={4}>
-                    <Box as="h3" textStyle="h3">
-                        SignUp
-                    </Box>
-                </Box>
-                <Box my={4} textAlign="left">
-                    <form onSubmit={props.handleNewUser}>
-                        <FormControl isRequired>
-                            <FormLabel>Nome</FormLabel>
-                            <Input
-                                variant="filled"
-                                mb={3}
-                                type="text"
-                                name="name"
-                                focusBorderColor="purple.400"
-                                value={props.name}
-                                onChange={(e) => {
-                                    props.setName(e.target.value);
-                                    props.setErrMsg('');
-                                }}
-                                placeholder="nome"
-                                disabled={props.loading}
-                                required
-                            />
-                        </FormControl>
-                        <FormControl isRequired>
-                            <FormLabel>Email</FormLabel>
-                            <Input
-                                variant="filled"
-                                mb={3}
-                                type="email"
-                                name="email"
-                                focusBorderColor="purple.400"
-                                value={props.email}
-                                onChange={(e) => {
-                                    props.setEmail(e.target.value);
-                                    props.setErrMsg('');
-                                }}
-                                placeholder="email"
-                                disabled={props.loading}
-                                required
-                            />
-                        </FormControl>
-                        <FormControl isRequired>
-                            <FormLabel>Senha</FormLabel>
-                            <Input
-                                variant="filled"
-                                mb={3}
-                                type="password"
-                                name="password"
-                                focusBorderColor="purple.400"
-                                value={props.password}
-                                onChange={(e) => {
-                                    props.setPassword(e.target.value);
-                                    props.setErrMsg('');
-                                }}
-                                placeholder="senha"
-                                disabled={props.loading}
-                                required
-                            />
-                        </FormControl>
-                        <Button
-                            variant="solid"
-                            colorScheme="purple"
-                            width="full"
-                            mb={6}
-                            type="submit"
-                            disabled={props.loading}
-                        >
-                            {props.loading ? (
-                                <CircularProgress
-                                    isIndeterminate
-                                    size={8}
-                                    color="purple"
-                                />
-                            ) : (
-                                'Cadastrar'
-                            )}
-                        </Button>
-                    </form>
-                </Box>
+        <Box layerStyle="credentialsCard">
+            <Box
+                as="h2"
+                textStyle="h2"
+                textAlign="center"
+                m={4}
+                fontWeight="600"
+            >
+                Cadastro
             </Box>
-        </Flex>
+            <form onSubmit={props.handleNewUser}>
+                <VStack spacing={8}>
+                    <FormControl isRequired>
+                        <FormLabel as="h3" textStyle="h3">
+                            Nome
+                        </FormLabel>
+                        <Input
+                            type="text"
+                            name="name"
+                            focusBorderColor="#3db9a2"
+                            value={props.name}
+                            onChange={(e) => {
+                                props.setName(e.target.value);
+                                props.setErrMsg('');
+                            }}
+                            placeholder="nome"
+                            disabled={props.loading}
+                            required
+                        />
+                    </FormControl>
+                    <FormControl isRequired>
+                        <FormLabel as="h3" textStyle="h3">
+                            Email
+                        </FormLabel>
+                        <Input
+                            type="email"
+                            name="email"
+                            focusBorderColor="#3db9a2"
+                            value={props.email}
+                            onChange={(e) => {
+                                props.setEmail(e.target.value);
+                                props.setErrMsg('');
+                            }}
+                            placeholder="email"
+                            disabled={props.loading}
+                            required
+                        />
+                    </FormControl>
+                    <FormControl isRequired>
+                        <FormLabel as="h3" textStyle="h3">
+                            Senha
+                        </FormLabel>
+                        <Input
+                            type="password"
+                            name="password"
+                            focusBorderColor="#3db9a2"
+                            value={props.password}
+                            onChange={(e) => {
+                                props.setPassword(e.target.value);
+                                props.setErrMsg('');
+                            }}
+                            placeholder="senha"
+                            disabled={props.loading}
+                            required
+                        />
+                    </FormControl>
+                    <Button type="submit" w="full" disabled={props.loading}>
+                        {props.loading ? (
+                            <CircularProgress
+                                isIndeterminate
+                                size={8}
+                                color="purple"
+                            />
+                        ) : (
+                            'Cadastrar'
+                        )}
+                    </Button>
+                </VStack>
+            </form>
+        </Box>
     );
 }
