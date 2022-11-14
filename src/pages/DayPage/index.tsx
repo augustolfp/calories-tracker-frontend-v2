c392c5abf672447ef0858301d769c14f521777e7
Augusto Lopes <augustolfp@gmail.com>
2022-11-14 11:50:37 -0300

    feat(day resume card): Add responsiveness to page elements

--- src/pages/DayPage/index.tsx
+++ src/pages/DayPage/index.tsx
@@ -1,5 +1,5 @@
 import { useData } from '../../hooks/useData';
-import { Container, Box } from '@chakra-ui/react';
+import { Container, Box, Flex } from '@chakra-ui/react';
 import { useParams } from 'react-router-dom';
 import MealsTab from './MealsTab';
 import DayResumeCard from '../../components/DayResumeCard';
@@ -13,13 +13,27 @@ export default function DayPage() {
     const formattedDate = formatDate(dayData!.day);
 
     return (
-        <Container maxW="900px" centerContent py="40px">
+        <Container maxW="1200px" centerContent py="40px">
             {dayData && (
                 <>
-                    <Box>
-                        <Box>{dayData.notes}</Box>
+                    <Flex
+                        direction={['column', 'column', 'row']}
+                        h={['auto', 'auto', '244px']}
+                        w={['308px', '400px', '768px', '960px']}
+                        mb={['10px', '20px']}
+                    >
+                        <Box
+                            bg="blue"
+                            w="full"
+                            mb={['10px', '20px', '0']}
+                            mr={['0', '0', '20px']}
+                        >
+                            <Box>{dayData.notes}</Box>
+                            <Box>{formattedDate.weekDay}</Box>
+                            <Box>{formattedDate.fullDate}</Box>
+                        </Box>
                         <DayResumeCard {...dayData} />
-                    </Box>
+                    </Flex>
                     <Box layerStyle="tabBox">
                         <MealsTab meals={dayData.dayMeals} dayId={Number(id)} />
                     </Box>
