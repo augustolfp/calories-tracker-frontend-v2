efd9c4b0bd286504d4539023f6886dabded67db8
Augusto Lopes <augustolfp@gmail.com>
2022-11-21 14:21:21 -0300

    feat(Dashboard): Add styling to day creation form

--- src/components/DayForm/index.tsx
+++ src/components/DayForm/index.tsx
@@ -6,24 +6,28 @@ import {
     FormLabel,
     CircularProgress,
     Box,
+    Grid,
+    GridItem,
+    Flex,
 } from '@chakra-ui/react';
 import { SingleDatepicker } from 'chakra-dayzed-datepicker';
-import {
-    NumberInput,
-    NumberInputField,
-    NumberInputStepper,
-    NumberIncrementStepper,
-    NumberDecrementStepper,
-} from '@chakra-ui/react';
 import { useDayCreator } from '../../hooks/useDayCreator';
+
+import {
+    Slider,
+    SliderTrack,
+    SliderFilledTrack,
+    SliderThumb,
+    SliderMark,
+} from '@chakra-ui/react';
 
 export default function DayForm() {
     const { mutate: newDay, isLoading, isError, error } = useDayCreator();
 
     const [date, setDate] = useState(new Date());
     const [notes, setNotes] = useState('');
-    const [caloriesTarget, setCaloriesTarget] = useState('');
-    const [proteinsTarget, setProteinsTarget] = useState('');
+    const [caloriesTarget, setCaloriesTarget] = useState(2000);
+    const [proteinsTarget, setProteinsTarget] = useState(100);
 
     async function handleNewDay(e: any) {
         e.preventDefault();
@@ -38,70 +42,145 @@ export default function DayForm() {
 
     return (
         <form onSubmit={handleNewDay}>
-            <FormControl isRequired>
-                <FormLabel>Data</FormLabel>
-                <SingleDatepicker
-                    configs={{
-                        dayNames: 'Dom Seg Ter Qua Qui Sex Sab'.split(' '),
-                        monthNames:
-                            'Jan Fev Mar Abr Mai Jun Jul Ago Set Out Nov Dez'.split(
-                                ' '
-                            ),
-                        dateFormat: 'dd-MM-yyyy',
-                    }}
-                    name="date-input"
-                    date={date}
-                    onDateChange={setDate}
-                />
-            </FormControl>
-            <FormControl>
-                <FormLabel>Notas</FormLabel>
-                <Input
-                    type="text"
-                    name="notes"
-                    value={notes}
-                    onChange={(e) => {
-                        setNotes(e.target.value);
-                    }}
-                    placeholder="Insira uma anotação"
-                    disabled={isLoading}
-                />
-            </FormControl>
-            <FormControl isRequired>
-                <FormLabel>Proteinas</FormLabel>
-                <Input
-                    type="number"
-                    name="proteinsTarget"
-                    value={proteinsTarget}
-                    onChange={(e) => {
-                        setProteinsTarget(e.target.value);
-                    }}
-                    placeholder="Alvo de proteinas (em g)"
-                    disabled={isLoading}
-                    required
-                />
-            </FormControl>
-            <FormControl isRequired>
-                <FormLabel>Calorias</FormLabel>
-                <Input
-                    type="number"
-                    name="caloriesTarget"
-                    value={caloriesTarget}
-                    onChange={(e) => {
-                        setCaloriesTarget(e.target.value);
-                    }}
-                    placeholder="Alvo de kCals"
-                    disabled={isLoading}
-                    required
-                />
-            </FormControl>
-            <Button type="submit" disabled={isLoading}>
-                {isLoading ? (
-                    <CircularProgress isIndeterminate size={8} color="purple" />
-                ) : (
-                    'Adicionar'
-                )}
-            </Button>
+            <Grid
+                templateAreas={`"notes date"
+                                "kcals kcals"
+                                "proteins proteins"
+                                "button button"`}
+                gridTemplateColumns="1fr 1fr"
+                gridTemplateRows="1.5fr 1fr 1fr 1fr"
+                gridGap={['10px', '20px']}
+            >
+                <GridItem
+                    area="notes"
+                    layerStyle="colorfulCard"
+                    bg="pageGreen.500"
+                    px="20px"
+                >
+                    <FormControl>
+                        <FormLabel color="white" fontWeight="700">
+                            Anotações
+                        </FormLabel>
+                        <Input
+                            type="text"
+                            name="notes"
+                            value={notes}
+                            onChange={(e) => {
+                                setNotes(e.target.value);
+                            }}
+                            placeholder="Escreva aqui..."
+                            disabled={isLoading}
+                        />
+                    </FormControl>
+                </GridItem>
+                <GridItem
+                    area="date"
+                    layerStyle="colorfulCard"
+                    bg="pageGreen.500"
+                    px="20px"
+                >
+                    <FormControl isRequired>
+                        <FormLabel color="white" fontWeight="700">
+                            Data
+                        </FormLabel>
+                        <SingleDatepicker
+                            configs={{
+                                dayNames: 'Dom Seg Ter Qua Qui Sex Sab'.split(
+                                    ' '
+                                ),
+                                monthNames:
+                                    'Jan Fev Mar Abr Mai Jun Jul Ago Set Out Nov Dez'.split(
+                                        ' '
+                                    ),
+                                dateFormat: 'dd-MM-yyyy',
+                            }}
+                            name="date-input"
+                            date={date}
+                            onDateChange={setDate}
+                        />
+                    </FormControl>
+                </GridItem>
+                <GridItem
+                    area="kcals"
+                    layerStyle="colorfulCard"
+                    bg="kcalsColor.500"
+                    py="10px"
+                    px="20px"
+                >
+                    <FormControl isRequired>
+                        <Flex justifyContent="space-between" gap="20px">
+                            <Box w="full">
+                                <FormLabel color="white" fontWeight="700">
+                                    Alvo de Calorias
+                                </FormLabel>
+                                <Slider
+                                    aria-label="Definir alvo de calorias"
+                                    defaultValue={caloriesTarget}
+                                    min={1000}
+                                    max={6000}
+                                    step={50}
+                                    onChange={(val) => setCaloriesTarget(val)}
+                                >
+                                    <SliderTrack bg="white">
+                                        <SliderFilledTrack bg="pageGreen.500" />
+                                    </SliderTrack>
+                                    <SliderThumb />
+                                </Slider>
+                            </Box>
+                            <Flex direction="column">
+                                <Box>{caloriesTarget}</Box>
+                                <Box>kCal</Box>
+                            </Flex>
+                        </Flex>
+                    </FormControl>
+                </GridItem>
+                <GridItem
+                    area="proteins"
+                    layerStyle="colorfulCard"
+                    bg="proteinsColor.500"
+                    px="20px"
+                >
+                    <FormControl isRequired>
+                        <Flex justifyContent="space-between" gap="20px">
+                            <Box w="full">
+                                <FormLabel color="white" fontWeight="700">
+                                    Proteinas
+                                </FormLabel>
+                                <Slider
+                                    aria-label="Definir alvo de proteinas"
+                                    defaultValue={proteinsTarget}
+                                    min={20}
+                                    max={300}
+                                    step={5}
+                                    onChange={(val) => setProteinsTarget(val)}
+                                >
+                                    <SliderTrack bg="white">
+                                        <SliderFilledTrack bg="pageGreen.500" />
+                                    </SliderTrack>
+                                    <SliderThumb />
+                                </Slider>
+                            </Box>
+                            <Flex direction="column">
+                                <Box>{proteinsTarget}</Box>
+                                <Box>g</Box>
+                            </Flex>
+                        </Flex>
+                    </FormControl>
+                </GridItem>
+                <GridItem area="button">
+                    <Button w="full" type="submit" disabled={isLoading}>
+                        {isLoading ? (
+                            <CircularProgress
+                                isIndeterminate
+                                size={8}
+                                color="purple"
+                            />
+                        ) : (
+                            'Adicionar'
+                        )}
+                    </Button>
+                </GridItem>
+            </Grid>
         </form>
     );
 }
