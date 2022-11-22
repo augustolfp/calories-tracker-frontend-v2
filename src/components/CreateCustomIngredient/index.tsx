0a5faba26c7a58828ccf11ca66b6200574cd8138
Augusto Lopes <augustolfp@gmail.com>
2022-11-22 11:58:28 -0300

    fix(Day page): Create custom ingredient form validation

--- src/components/CreateCustomIngredient/index.tsx
+++ src/components/CreateCustomIngredient/index.tsx
@@ -59,215 +59,214 @@ export default function CreateCustomIngredient(props: Props) {
             <Box as="h3" textStyle="h3" fontWeight="600">
                 Crie um ingrediente
             </Box>
-            <form onSubmit={handleIngredientCreation}>
-                <Grid
-                    templateAreas={`"name name"
+            <Grid
+                templateAreas={`"name name"
                                     "quantity carbs" 
                                     "proteins fats"
                                     "calories createBtn"`}
-                    gridTemplateColumns="1fr 1fr"
-                    gridGap="10px"
+                gridTemplateColumns="1fr 1fr"
+                gridGap="10px"
+            >
+                <GridItem
+                    area={'name'}
+                    layerStyle="colorfulCard"
+                    bg="pageGreen.500"
+                    p={4}
                 >
-                    <GridItem
-                        area={'name'}
-                        layerStyle="colorfulCard"
-                        bg="pageGreen.500"
-                        p={4}
-                    >
-                        <FormControl isRequired>
-                            <FormLabel fontWeight="700">Nome</FormLabel>
-                            <Input
-                                type="text"
-                                name="ingName"
-                                variant="unstyled"
-                                value={name}
-                                onChange={(e) => {
-                                    setName(e.target.value);
-                                }}
-                                placeholder="Digite aqui..."
-                                disabled={isLoading}
-                                required
-                            />
-                        </FormControl>
-                    </GridItem>
-                    <GridItem area={'quantity'}>
-                        <FormControl isRequired>
-                            <FormLabel
-                                color="white"
-                                fontWeight="700"
-                                position="absolute"
-                                top="4px"
-                                left="50%"
-                                sx={{
-                                    'z-index': '1',
-                                    transform: 'translate(-50%,0)',
-                                }}
-                            >
-                                Quantidade
-                            </FormLabel>
-                            <NumberInput
-                                variant="ingredientVariant"
-                                bg="pageGreen.500"
-                                value={formatNumToGrams(weight)}
-                                step={5}
-                                onChange={(value) => {
-                                    setWeight(parseGramsToNumber(value));
-                                }}
-                                isDisabled={isLoading}
-                                isRequired
-                            >
-                                <NumberInputField />
-                                <NumberInputStepper>
-                                    <NumberIncrementStepper />
-                                    <NumberDecrementStepper />
-                                </NumberInputStepper>
-                            </NumberInput>
-                        </FormControl>
-                    </GridItem>
-                    <GridItem area={'carbs'}>
-                        <FormControl isRequired>
-                            <FormLabel
-                                color="white"
-                                fontWeight="700"
-                                position="absolute"
-                                top="4px"
-                                left="50%"
-                                sx={{
-                                    'z-index': '1',
-                                    transform: 'translate(-50%,0)',
-                                }}
-                            >
-                                Carboidratos
-                            </FormLabel>
-                            <NumberInput
-                                variant="ingredientVariant"
-                                bg="carbsColor.500"
-                                value={formatNumToGrams(carbs)}
-                                step={5}
-                                onChange={(value) => {
-                                    setCarbs(parseGramsToNumber(value));
-                                }}
-                                isDisabled={isLoading}
-                                isRequired
-                            >
-                                <NumberInputField />
-                                <NumberInputStepper>
-                                    <NumberIncrementStepper />
-                                    <NumberDecrementStepper />
-                                </NumberInputStepper>
-                            </NumberInput>
-                        </FormControl>
-                    </GridItem>
-                    <GridItem area={'fats'}>
-                        <FormControl isRequired>
-                            <FormLabel
-                                color="white"
-                                fontWeight="700"
-                                position="absolute"
-                                top="4px"
-                                left="50%"
-                                sx={{
-                                    'z-index': '1',
-                                    transform: 'translate(-50%,0)',
-                                }}
-                            >
-                                Gorduras
-                            </FormLabel>
-                            <NumberInput
-                                variant="ingredientVariant"
-                                bg="fatsColor.500"
-                                value={formatNumToGrams(fats)}
-                                step={5}
-                                onChange={(value) => {
-                                    setFats(parseGramsToNumber(value));
-                                }}
-                                isDisabled={isLoading}
-                                isRequired
-                            >
-                                <NumberInputField />
-                                <NumberInputStepper>
-                                    <NumberIncrementStepper />
-                                    <NumberDecrementStepper />
-                                </NumberInputStepper>
-                            </NumberInput>
-                        </FormControl>
-                    </GridItem>
-                    <GridItem area={'proteins'}>
-                        <FormControl isRequired>
-                            <FormLabel
-                                color="white"
-                                fontWeight="700"
-                                position="absolute"
-                                top="4px"
-                                left="50%"
-                                sx={{
-                                    'z-index': '1',
-                                    transform: 'translate(-50%,0)',
-                                }}
-                            >
-                                Proteinas
-                            </FormLabel>
-                            <NumberInput
-                                variant="ingredientVariant"
-                                bg="proteinsColor.500"
-                                value={formatNumToGrams(proteins)}
-                                step={5}
-                                onChange={(value) => {
-                                    setProteins(parseGramsToNumber(value));
-                                }}
-                                isDisabled={isLoading}
-                                isRequired
-                            >
-                                <NumberInputField />
-                                <NumberInputStepper>
-                                    <NumberIncrementStepper />
-                                    <NumberDecrementStepper />
-                                </NumberInputStepper>
-                            </NumberInput>
-                        </FormControl>
-                    </GridItem>
-                    <GridItem area={'calories'}>
-                        <Flex
-                            layerStyle="colorfulCard"
-                            bg="kcalsColor.500"
-                            display="flex"
-                            alignItems="center"
-                            h="60px"
-                        >
-                            <Flex
-                                direction="column"
-                                align="center"
-                                justify="center"
-                            >
-                                <Box>Calorias</Box>
-                                <Box fontWeight="400">(calculado)</Box>
-                            </Flex>
-                            <Box>{kcals}kCal</Box>
-                        </Flex>
-                    </GridItem>
-                    <GridItem area={'createBtn'}>
-                        <Button
-                            h="60px"
-                            variant="solid"
-                            colorScheme="purple"
-                            width="full"
-                            mb={6}
-                            type="submit"
+                    <FormControl isRequired>
+                        <FormLabel fontWeight="700">Nome</FormLabel>
+                        <Input
+                            type="text"
+                            name="ingName"
+                            variant="unstyled"
+                            value={name}
+                            onChange={(e) => {
+                                setName(e.target.value);
+                            }}
+                            placeholder="Digite aqui..."
                             disabled={isLoading}
+                            required
+                        />
+                    </FormControl>
+                </GridItem>
+                <GridItem area={'createBtn'}>
+                    <Button
+                        h="60px"
+                        variant="solid"
+                        colorScheme="purple"
+                        width="full"
+                        mb={6}
+                        type="submit"
+                        disabled={isLoading}
+                        onClick={handleIngredientCreation}
+                    >
+                        {isLoading ? (
+                            <CircularProgress
+                                isIndeterminate
+                                size={8}
+                                color="purple"
+                            />
+                        ) : (
+                            'Criar'
+                        )}
+                    </Button>
+                </GridItem>
+                <GridItem area={'quantity'}>
+                    <FormControl isRequired>
+                        <FormLabel
+                            color="white"
+                            fontWeight="700"
+                            position="absolute"
+                            top="4px"
+                            left="50%"
+                            sx={{
+                                'z-index': '1',
+                                transform: 'translate(-50%,0)',
+                            }}
                         >
-                            {isLoading ? (
-                                <CircularProgress
-                                    isIndeterminate
-                                    size={8}
-                                    color="purple"
-                                />
-                            ) : (
-                                'Criar'
-                            )}
-                        </Button>
-                    </GridItem>
-                </Grid>
-            </form>
+                            Quantidade
+                        </FormLabel>
+                        <NumberInput
+                            variant="ingredientVariant"
+                            bg="pageGreen.500"
+                            value={formatNumToGrams(weight)}
+                            step={5}
+                            onChange={(value) => {
+                                setWeight(parseGramsToNumber(value));
+                            }}
+                            isDisabled={isLoading}
+                            isRequired
+                        >
+                            <NumberInputField />
+                            <NumberInputStepper>
+                                <NumberIncrementStepper />
+                                <NumberDecrementStepper />
+                            </NumberInputStepper>
+                        </NumberInput>
+                    </FormControl>
+                </GridItem>
+                <GridItem area={'carbs'}>
+                    <FormControl isRequired>
+                        <FormLabel
+                            color="white"
+                            fontWeight="700"
+                            position="absolute"
+                            top="4px"
+                            left="50%"
+                            sx={{
+                                'z-index': '1',
+                                transform: 'translate(-50%,0)',
+                            }}
+                        >
+                            Carboidratos
+                        </FormLabel>
+                        <NumberInput
+                            variant="ingredientVariant"
+                            bg="carbsColor.500"
+                            value={formatNumToGrams(carbs)}
+                            step={5}
+                            onChange={(value) => {
+                                setCarbs(parseGramsToNumber(value));
+                            }}
+                            isDisabled={isLoading}
+                            isRequired
+                        >
+                            <NumberInputField />
+                            <NumberInputStepper>
+                                <NumberIncrementStepper />
+                                <NumberDecrementStepper />
+                            </NumberInputStepper>
+                        </NumberInput>
+                    </FormControl>
+                </GridItem>
+                <GridItem area={'fats'}>
+                    <FormControl isRequired>
+                        <FormLabel
+                            color="white"
+                            fontWeight="700"
+                            position="absolute"
+                            top="4px"
+                            left="50%"
+                            sx={{
+                                'z-index': '1',
+                                transform: 'translate(-50%,0)',
+                            }}
+                        >
+                            Gorduras
+                        </FormLabel>
+                        <NumberInput
+                            variant="ingredientVariant"
+                            bg="fatsColor.500"
+                            value={formatNumToGrams(fats)}
+                            step={5}
+                            onChange={(value) => {
+                                setFats(parseGramsToNumber(value));
+                            }}
+                            isDisabled={isLoading}
+                            isRequired
+                        >
+                            <NumberInputField />
+                            <NumberInputStepper>
+                                <NumberIncrementStepper />
+                                <NumberDecrementStepper />
+                            </NumberInputStepper>
+                        </NumberInput>
+                    </FormControl>
+                </GridItem>
+                <GridItem area={'proteins'}>
+                    <FormControl isRequired>
+                        <FormLabel
+                            color="white"
+                            fontWeight="700"
+                            position="absolute"
+                            top="4px"
+                            left="50%"
+                            sx={{
+                                'z-index': '1',
+                                transform: 'translate(-50%,0)',
+                            }}
+                        >
+                            Proteinas
+                        </FormLabel>
+                        <NumberInput
+                            variant="ingredientVariant"
+                            bg="proteinsColor.500"
+                            value={formatNumToGrams(proteins)}
+                            step={5}
+                            onChange={(value) => {
+                                setProteins(parseGramsToNumber(value));
+                            }}
+                            isDisabled={isLoading}
+                            isRequired
+                        >
+                            <NumberInputField />
+                            <NumberInputStepper>
+                                <NumberIncrementStepper />
+                                <NumberDecrementStepper />
+                            </NumberInputStepper>
+                        </NumberInput>
+                    </FormControl>
+                </GridItem>
+                <GridItem area={'calories'}>
+                    <Flex
+                        layerStyle="colorfulCard"
+                        bg="kcalsColor.500"
+                        display="flex"
+                        alignItems="center"
+                        h="60px"
+                    >
+                        <Flex
+                            direction="column"
+                            align="center"
+                            justify="center"
+                        >
+                            <Box>Calorias</Box>
+                            <Box fontWeight="400">(calculado)</Box>
+                        </Flex>
+                        <Box>{kcals}kCal</Box>
+                    </Flex>
+                </GridItem>
+            </Grid>
         </Box>
     );
 }
--- src/components/IngredientForm/index.tsx
+++ src/components/IngredientForm/index.tsx
@@ -1,5 +1,6 @@
 import SearchIngredients from '../SearchIngredients';
 import CreateCustomIngredient from '../CreateCustomIngredient';
+import { Flex } from '@chakra-ui/react';
 
 type Props = {
     mealId: number;
@@ -9,6 +10,9 @@ export default function IngredientForm(props: Props) {
     return (
         <>
             <SearchIngredients mealId={props.mealId} />
+            <Flex w="full" justify="center" textStyle="h2">
+                Ou
+            </Flex>
             <CreateCustomIngredient mealId={props.mealId} />
         </>
     );
