d8d4b4aa27bfd58dbd7ef9d4025aabe229ecb04b
Augusto Lopes <augustolfp@gmail.com>
2022-11-26 18:37:01 -0300

    fix(Day page): Change conditional rendering rules

--- src/pages/DayPage/index.tsx
+++ src/pages/DayPage/index.tsx
@@ -4,17 +4,30 @@ import { useParams } from 'react-router-dom';
 import MealsTab from '../../components/MealsTab';
 import formatDate from '../../utils/formatDate';
 import Header from './Header';
+import { Day } from '../../types';
+
+type FormattedDateType = {
+    fullDate: string;
+    weekDay: string;
+};
 
 export default function DayPage() {
     const { data } = useData();
     const { id } = useParams();
+    let dayData: Day | undefined = undefined;
+    let formattedDate: FormattedDateType | undefined = undefined;
 
-    const dayData = data!.find((obj) => obj.id === Number(id));
-    const formattedDate = formatDate(dayData!.day);
+    if (data) {
+        dayData = data.find((obj) => obj.id === Number(id));
+    }
+
+    if (dayData) {
+        formattedDate = formatDate(dayData.day);
+    }
 
     return (
         <Container maxW="1200px" h="100vh" centerContent py="40px" gap="40px">
-            {dayData && (
+            {dayData && formattedDate ? (
                 <>
                     <Header
                         day={dayData}
@@ -25,7 +38,7 @@ export default function DayPage() {
                         <MealsTab meals={dayData.dayMeals} dayId={Number(id)} />
                     </Box>
                 </>
-            )}
+            ) : null}
         </Container>
     );
 }
